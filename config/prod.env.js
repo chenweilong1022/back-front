04e75df08f6d0fdf4a37052c0c2bc65d9334fe3e
chenweilong <chenweilong>
2020-01-20 17:14:37 +0800

    活动相关代码提交

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"http://180.150.227.102:2333"', // 东盟
-  BASE_API: '"http://HTTP_BASE_API"', // 955_test
+  BASE_API: '"http://39.108.50.148:8080"', // 955_test
   // BASE_API: '"http://61.97.249.37:9001"', // 955_test
   // BASE_API: '"http://27.255.91.77:2333"', // 955_prod
   // BASE_API: '"http://61.14.210.148:2333"', // 13水1
